a078e6b1d520df1532b2c029f34d7a4298f3c867
harut <har17bar@gmail.com>
2020-02-19 20:27:29 +0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import fs from 'fs'
+import path from 'path'
+import React from "react";
+import ReactDOMServer from 'react-dom/server'
+import App from '../src/App'
+
+const PORT = 8000;
+const app = express();
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use('^/$', (req, res, next) => {
+    fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data)=>{
+        if (err){
+            console.log(err);
+            return res.status(500).send("Some error happened")
+        }
+        return res.send(data.replace(
+            '<div id ="root"</div>',
+            `<div id ="root">${ReactDOMServer.renderToString(<App  />)}</div>`))
+    })
+});
+
+app.get('/har', (req, res) => res.json({a: "15"}));
+
+app.use(express.static(path.resolve(__dirname, '..','build')));
+
+app.listen(PORT, ()=>{
+    console.log(`App launched on ${PORT}`)
+});
--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+const fetch = require("node-fetch");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const fetchApp = (url) => {
+  return fetch(url).then(response => {
+    if(response.ok) {
+      return response.json()
+    } else {
+      return "Error"
+    }
+  }, failResponse => {
+    return "Failed"
+  })
+}
+
+function App(props) {
+  const [res, setRes] = useState()
+   fetchApp('http://127.0.0.1:8000/har').then(response => {
+       console.log(response)
+     setRes(response)
+   })
+
+    console.log(props.customComp,"-infqnaa")
+
+    return (
+        <div className="App">
+           <div className={"xachik"}>{ props.customComp }</div>
+          <header className="App-header">
+            <p>{res && res.a}</p>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    );
 }
 
 export default App;
